ed84c222f2c01ceb5861783d45a4a4fa73be2da7
Aaron Bieber <airborne@Yelena.local>
2009-12-01 20:56:36 -0500

    Permit the api action "edit," which allows the user to preview and edit what they're sending from the bookmarklet.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -16,4 +16,22 @@ class ApiController < ApplicationController
 		end
 	end
 
+  def edit
+		if !params[:p].nil? and !params[:p].empty?
+			text = params[:p]
+			if !params[:u].nil? and !params[:u].empty?
+				text += "\n\nPasted from "+params[:u]
+			end
+			@paste = Paste.new({ :paste => text })
+			@paste.setup
+      @paste.ip = request.remote_ip
+
+      render :template => 'main/paste'
+			#if p.valid?
+			#	p.save
+			#	redirect_to :controller => 'main', :action => 'view', :code => p.code
+			#end
+		end
+  end
+
 end
