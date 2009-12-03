fbab275d60db8956af291ac6f1d15afb5db2fe86
Aaron Bieber <airborne@orion.(none)>
2009-12-03 09:40:23 -0500

    Removed the multi-environment deploy config (now deploying only to the new server), and updated the stylesheet to work better with blockquotes.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,4 @@
 set :application, "pastehost"
 set :repository, "git://github.com/aaronbieber/pastehost.git"
-
-task :new do
-	set :domain, "diagramwar.com"
-	set :deploy_to, "/var/www/pastehost.com/pastehost"
-end
-
-task :current do
-	set :domain, "pastehost.com"
-	set :deploy_to, "/var/www/pastehost.com/pastehost"
-end
+set :domain, "pastehost.com"
+set :deploy_to, "/var/www/pastehost.com/pastehost"
