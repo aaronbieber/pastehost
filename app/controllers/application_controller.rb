e82aef904161c9d7cea374974a0e11cb57e5e77f
Aaron Bieber <aaron@aaronbieber.com>
2010-01-12 07:28:59 -0500

    Mobile / iPhone version.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,34 @@
 
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
-  session :session_key => '_pastehost_session_id'
+  # session :session_key => '_pastehost_session_id'
+  layout :detect_browser
+
+  private
+  MOBILE_BROWSERS = [
+    "android",                  "ipod",
+    "opera mini",               "blackberry",
+    "palm",                     "hiptop",
+    "avantgo",                  "plucker",
+    "xiino",                    "blazer",
+    "elaine",                   "windows ce; ppc;",
+    "windows ce; smartphone;",  "windows ce; iemobile",
+    "up.browser",               "up.link",
+    "mmp",                      "symbian",
+    "smartphone",               "midp",
+    "wap",                      "vodafone",
+    "o2",                       "pocket",
+    "kindle",                   "mobile",
+    "pda",                      "psp",
+    "treo"
+  ]
+
+  def detect_browser
+    agent = request.headers["HTTP_USER_AGENT"].downcase
+    MOBILE_BROWSERS.each do |m|
+      return "mobile_application" if agent.match(m)
+    end
+
+    return "application"
+  end
 end
